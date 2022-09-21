7c8788495efaad0a1c8bf788fa3bff8b407b8d7d
jethanstory <jstory@csu.fullerton.edu>
2022-09-21 12:36:09 -0700

    Added a basic grid system on a separate map for testing

---
+++ Project-Parking/Assets/Scripts/GridManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+    [SerializeField] private Tile _tilePrefab;
+
+    void Start() 
+    {
+    GenerateGrid();
+    }
+    // Start is called before the first frame update
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++) {
+            for (int y = 0; y < _height; y++) {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
