ddbcf1cd9ae104c7d5a39e0866a810da2aee7725
jethanstory <32407746+jethanstory@users.noreply.github.com>
2022-09-28 09:34:46 -0700

    Added speeding check for player so that the cop AI chases them if they speed

---
+++ Project-Parking/Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    public float damping;
+
+    public NavMeshAgent agent;
+    public NavMeshAgent fpsTarget;
+    public Transform player;
+
+    // Start is called before the first frame update
+
+    [Range(0, 100)] public float speed;
+    [Range(1, 500)] public float walkRadius;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (agent != null)
+        {
+            agent.speed = speed;
+            agent.SetDestination(player.position);
+        }
+    }
+
+    /*public Vector3 RandomNavMeshLocation()
+    {
+        Vector3 finalPosition = Vector3.zero;
+        Vector3 randomPosition = Random.insideUnitSphere * walkRadius;
+        randomPosition += transform.position;
+        if (NavMesh.SamplePosition(randomPosition, out NavMeshHit hit, walkRadius, 1)) {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    } */
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * damping);
+        
+        //transform.LookAt(player);
+        agent.SetDestination(player.position);
+        
+
+    }
+}
---
+++ Project-Parking/Assets/Scripts/SpaceFinderAI.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceFinderAI : MonoBehaviour
+{
+    public float fpsTargetDistance;
+    public float enemyLookDistance;
+    public float attackDistance;
+    public float enemyMovementSpeed;
+    public float damping;
+
+    public GameObject lightSource;
+    public Transform fpsTarget;
+    public Transform fpsWanderTarget;
+    Rigidbody theRigidBody;
+
+    //public Transform stuckCheck;
+    //Renderer myRenderer;
+
+    private bool lightEnabled;
+    //public GameObject[] sounds;
+    public GameObject[] lights;
+
+    //public bool timerStarted = true;
+    public float timer = 0.0f;
+
+    //public AudioSource audioSource;
+
+    //private Light enemyLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+       //myRenderer = GetComponent<Renderer>();
+       theRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //resetSystem();
+        fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+        if (fpsTargetDistance < enemyLookDistance) {
+            //myRenderer.material.color = Color.yellow;
+            //Disables the Advanced Wander AI script and the NavMeshAgent script so the enemy stops when you are in range. 
+            GameObject.Find("CarAI").GetComponent<AdvancedWanderAI>().enabled = false;
+            GameObject.Find("CarAI").GetComponent<FollowingEnemy>().enabled = true;
+            //gameObject.GetComponent<NavMeshAgent>().enabled = false;
+            //lookAtPlayer();
+            
+            //if (fpsTargetDistance < attackDistance) {
+                //GameObject.Find("WanderingEnemy").GetComponent<FollowingEnemy>().enabled = false;
+                //GameObject.Find("WanderingEnemy").GetComponent<AttackPlayer>().enabled = true;
+                //myRenderer.material.color = Color.red;
+                //attackPlease();
+            //}
+        }
+        
+        else{
+            GameObject.Find("CarAI").GetComponent<FollowingEnemy>().enabled = false;
+            GameObject.Find("CarAI").GetComponent<AdvancedWanderAI>().enabled = true;
+            //gameObject.GetComponent<NavMeshAgent>().enabled = true;
+            
+            
+            //Wander to player position
+            //WandertoPlacePlease();
+
+            //myRenderer.material.color = Color.blue;
+            //enemyLight.color = Color.white;
+          
+        }
+    }
+
+    // void resetSystem()
+    // {
+    //    // if(!timerStarted)
+    //     //{
+    //     //    timerStarted = true;
+    //     //    timer = 0.0f;
+    //     //}
+    //     //else
+    //     //{
+    //     timer += Time.deltaTime;
+    //     //}
+    //     if(timer >= 5.0f)
+    //     {
+    //         GameObject.Find("WanderingEnemy").GetComponent<AdvancedWanderAI>().enabled = false;
+    //         GameObject.Find("WanderingEnemy").GetComponent<FixerFollow>().enabled = true;
+    //         //timerStarted = false;
+    //         if(timer >= 7.0f)
+    //         {
+    //             timer = 0.0f;
+    //             GameObject.Find("WanderingEnemy").GetComponent<AdvancedWanderAI>().enabled = true;
+    //             GameObject.Find("WanderingEnemy").GetComponent<FixerFollow>().enabled = false;
+    //         }
+            
+    //     }
+            
+        //else
+        //{
+        //    timerStarted = false;
+        //}
+        
+    //}
+
+}
